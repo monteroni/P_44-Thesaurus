fa1a38a3020c434eeb6d788418b6c7a05e98c892
Fischer David <fischerda@etmlnet.local>
2018-01-30 16:23:27 +0100

    Réalisation de la détéction d'encoding et de la séparation de mot

--- Test David/Test K Google/Test K Google/Program.cs
+++ Test David/Test K Google/Test K Google/Program.cs
@@ -12,11 +12,12 @@ namespace Test_K_Google
 {
     class Program
     {
-        
+
 
         static void Main(string[] args)
         {
-            DirectoryInfo di = new DirectoryInfo(@"K:\INF\Eleves\Temp");
+            DirectoryInfo di = new DirectoryInfo(@"K:\INF\Eleves\Temp\Thesaurus");
+            #region
             /*Console.WriteLine("No search pattern returns:");
             foreach (var fi in di.GetFiles())
             {
@@ -25,10 +26,10 @@ namespace Test_K_Google
 
             Console.WriteLine();*/
 
-      //      string text = System.IO.File.ReadAllText(@"K:\INF\Eleves\Temp\Arborescence-Projet-Web.bat");
+            //      string text = System.IO.File.ReadAllText(@"K:\INF\Eleves\Temp\Arborescence-Projet-Web.bat");
 
             // Display the file contents to the console. Variable text is a string.
-      //      System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            //      System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
 
             ////////////Application wordApp = new Application();
             ////////////Document word = wordApp.Documents.Open(_path);
@@ -43,7 +44,7 @@ namespace Test_K_Google
             ////////////text = text.Replace("\r\n", " ").Replace("\r", " ").Replace("\n", " ");
             ////////////string[] allWords = text.Split(' ');
 
-
+#endregion
 
 
             /*
@@ -62,19 +63,71 @@ namespace Test_K_Google
                 
             }
             */
-            Console.WriteLine();
-
+            // Console.WriteLine();
+            string _encoding = "1252";
             Console.WriteLine("Search pattern AllDirectories returns:");
             foreach (var fi in di.GetFiles("*.txt", SearchOption.AllDirectories))
             {
-                
-                string path = fi.DirectoryName +"\\"+ fi.Name;
+                using (FileStream fs = File.OpenRead(fi.FullName))
+                {
+                    Ude.CharsetDetector cdet = new Ude.CharsetDetector();
+                    cdet.Feed(fs);
+                    cdet.DataEnd();
+                    if (cdet.Charset != null)
+                    {
+                        _encoding = cdet.Charset;
+                        Console.WriteLine(_encoding);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Detection failed.");
+                    }
+                }
+
+                string path = fi.DirectoryName + "\\" + fi.Name;
                 Console.WriteLine(path);
-                string polpi = System.IO.File.ReadAllText(path);
-                Console.WriteLine(polpi);
-                Console.WriteLine();
+                StreamReader Reader = new StreamReader(path, Encoding.GetEncoding(_encoding));
+                string polpi = Reader.ReadToEnd();                                              
+                Reader.Close();
+
+
+
+
+
+
+
+
+                Char delimiter = ' ';
+                String[] substrings = polpi.Split(delimiter  , '\r');
                 
-                Console.WriteLine();
+
+                Array.Sort(substrings);
+
+
+
+                List<string> lstSub = new List<string>(substrings);
+
+
+
+
+
+                for (int i = 0; i < lstSub.Count; i++)
+                {
+                    lstSub[i] = lstSub[i].ToLower();
+                    if (lstSub[i][0] == ' ' || lstSub[i][0] == '!' || lstSub[i][0] == '?' || lstSub[i][0] == '.' || lstSub[i][0] == '\\')
+                    {
+                        
+                        lstSub.Remove(lstSub[i]);
+                    }
+                }
+
+
+
+                foreach (var substring in lstSub)
+                    Console.WriteLine(substring);
+
+
+
                 Thread.Sleep(400);
             }
         }
